fa813a78d5fe553e104010ca79151283e4f176e2
prazdevs <sachabouillez@gmail.com>
2021-10-22 12:30:55 +0200

    :construction: debug

--- src/index.ts
+++ src/index.ts
@@ -18,29 +18,30 @@ export default function httpsCerts (options: PluginOptions): Plugin {
     defaultIfNoCerts = true,
   } = options
 
-  const plugin = {
+  const plugin: Plugin = {
     name: 'dev-https-certs',
 
     config: (): UserConfig => {
       const root = process.cwd()
-      console.log(`Reading folder ${join(root, path)}`)
       const files = readdirSync(join(root, path))
-      console.log(`Found files ${files}`)
 
       const keyFile = files.find(f => certExts.includes(extname(f)))
       const certFile = files.find(f => keyExts.includes(extname(f)))
-      console.log(`Found key '${keyFile}' and cert '${certFile}'`)
 
       if (keyFile && certFile) {
-        // eslint-disable-next-line max-len
-        console.log(`Will load '${join(root, path, keyFile)}' and '${join(root, path, certFile)}'`)
         const key = join(root, path, keyFile)
         const cert = join(root, path, certFile)
 
-        return { server: { https: { key, cert } } }
+        const config = { server: { https: { key, cert } } }
+
+        return config
       }
 
       return { server: { https: defaultIfNoCerts } }
+    },
+
+    configResolved: config => {
+      console.log(config)
     },
   }
   return plugin as Plugin
