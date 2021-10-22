8e9259b9840c4dfa9c43772748479efdec162962
prazdevs <sachabouillez@gmail.com>
2021-10-22 12:14:49 +0200

    :loud_sound: add logs

--- src/index.ts
+++ src/index.ts
@@ -23,12 +23,17 @@ export default function httpsCerts (options: PluginOptions): Plugin {
 
     config: (): UserConfig => {
       const root = process.cwd()
+      console.log(`Reading folder ${join(root, path)}`)
       const files = readdirSync(join(root, path))
+      console.log(`Found files ${files}`)
 
       const keyFile = files.find(f => certExts.includes(extname(f)))
       const certFile = files.find(f => keyExts.includes(extname(f)))
+      console.log(`Found key '${keyFile}' and cert '${certFile}'`)
 
       if (keyFile && certFile) {
+        // eslint-disable-next-line max-len
+        console.log(`Will load '${join(root, path, keyFile)}' and '${join(root, path, keyFile)}'`)
         const key = readFileSync(join(root, path, keyFile))
         const cert = readFileSync(join(root, path, certFile))
 
